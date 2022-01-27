b9f96a3fcb0cc50e723a817d3f3f6f3f4a6f5835
naidak <57109234+naidak@users.noreply.github.com>
2022-01-27 18:00:47 +0100

    Update zadatak 4 - 15 infinity.cpp

--- zadatak 4 - 15 infinity.cpp
+++ zadatak 4 - 15 infinity.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
 
 struct fudbaler {
@@ -9,15 +8,16 @@ struct fudbaler {
 	int* broj_golova;
 
 	fudbaler() {
-		this->ime_i_Prezime = new char[50];
-		this->pozicija = new char;
-		this->broj_golova = new int;
+		ID = 0;
+		ime_i_Prezime = new char[40];
+		pozicija = new char;
+		broj_golova = new int;
 	}
 
 	~fudbaler() {
-		delete[] this->ime_i_Prezime; this->ime_i_Prezime = nullptr;
-		delete this->pozicija; this->pozicija = nullptr;
-		delete this->broj_golova; this->broj_golova = nullptr;
+		delete[] ime_i_Prezime; ime_i_Prezime = nullptr;
+		delete pozicija; pozicija = nullptr;
+		delete broj_golova; broj_golova = nullptr;
 	}
 };
 
@@ -25,126 +25,112 @@ void Unos(fudbaler** niz, int red, int kolona)
 {
 	for (int i = 0; i < red; i++)
 	{
-		cout << "Tim " << i + 1 << ":\n";
+		cout << "Klub " << i + 1 << endl;
 		for (int j = 0; j < kolona; j++)
 		{
-			cout << "Fudbaler " << j + 1 << ":\n";
+			cout << "Fudbaler " << j + 1 << endl;
 			cout << "Unesite ID: ";
 			cin >> (*(niz + i) + j)->ID;
 			cout << "Unesite ime i prezime: ";
-			cin.ignore();
-			cin.getline((*(niz + i) + j)->ime_i_Prezime,50);
-			do
-			{
-				cout << "Unesite poziciju (1 - O, 2 - N, 3 - V): ";
-				cin >> *(*(niz + i) + j)->pozicija;
-			} while (*(*(niz + i) + j)->pozicija!='N' && *(*(niz + i) + j)->pozicija!='V' && *(*(niz + i) + j)->pozicija!='O');
-		
+			cin >> (*(niz + i) + j)->ime_i_Prezime;
+			do{
+			cout << "Unesite poziciju: ";
+			cin >> *(*(niz + i) + j)->pozicija;
+			} while (*(*(niz + i) + j)->pozicija != 'N' && *(*(niz + i) + j)->pozicija != 'V' && *(*(niz + i) + j)->pozicija != 'O');
 			cout << "Unesite broj golova: ";
 			cin >> *(*(niz + i) + j)->broj_golova;
 		}
 	}
 }
 
-void Ispis(fudbaler** niz, int red, int kolona)
+void GoloviPoPoziciji(fudbaler** niz, int red, int kolona)
 {
-	for (int i = 0; i < red; i++)
-	{
-		for (int j = 0; j < kolona; j++)
-		{
-			cout << "Fudbaler " << j + 1 << ":\n";
-			cout << "ID: " << (*(niz + i) + j)->ID << endl;
-			cout << "Ime i prezime: " << (*(niz + i) + j)->ime_i_Prezime << endl;
-			cout << "Pozicija: " << *(*(niz + i) + j)->pozicija << endl;
-			cout << "Broj golova: " << *(*(niz + i) + j)->broj_golova << endl;
-		}
-	}
-}
-
-void Prosjek(fudbaler** niz, int red, int kolona)
-{
-	float* prosjeci = new float[3]{};
-	int* brojac = new int[3]{};
+	int brojacN = 0, brojacV = 0, brojacO = 0;
+	float prosjekN = 0.0, prosjekV = 0.0, prosjekO = 0.0;
 
 	for (int i = 0; i < red; i++)
 	{
 		for (int j = 0; j < kolona; j++)
 		{
-			if (*(*(niz + i) + j)->pozicija == 'O')
+			if (*(*(niz + i) + j)->pozicija == 'N')
 			{
-				prosjeci[0] += *(*(niz + i) + j)->broj_golova;
-				brojac[0]+=1;
+				brojacN++;
+				prosjekN += *(*(niz + i) + j)->broj_golova;
 			}
-			else if(*(*(niz + i) + j)->pozicija == 'N')
+			else if (*(*(niz + i) + j)->pozicija == 'V')
 			{
-				prosjeci[1] += *(*(niz + i) + j)->broj_golova;
-				brojac[1] += 1;
+				brojacV++;
+				prosjekV += *(*(niz + i) + j)->broj_golova;
 			}
 			else
 			{
-				prosjeci[2] += *(*(niz + i) + j)->broj_golova;
-				brojac[2] += 1;
+				brojacO++;
+				prosjekO += *(*(niz + i) + j)->pozicija;
 			}
-
 		}
 	}
-	for (int i = 0; i < 3; i++)
-	{
-		prosjeci[i] /= brojac[i];
-		cout << "Prosjek na za poziciju " << i + 1 << " je: " << prosjeci[i]<<endl;
-	}
+	cout << "Prosjek postignutih golova na poziciji N iznosi: " << prosjekN / brojacN << endl;
+	cout << "Prosjek postignutih golova na poziciji V iznosi: " << prosjekV / brojacV << endl;
+	cout << "Prosjek postignutih golova na poziciji O iznosi: " << prosjekO / brojacO << endl;
 }
 
-void Najbolji(fudbaler** niz, int red, int kolona,char izbor)
+void NajboljiProsjekPoPoziciji(fudbaler** niz, int red, int kolona,char izbor)
 {
 	float* prosjeci = new float[red] {};
-	int* brojaci = new int[red] {};
-	int indeks_max=0;
-	float prosjek_max = 0.0;
+
+	int indeks = 0;
+	float najbolji_prosjek = 0.0;
+
 	for (int i = 0; i < red; i++)
 	{
+		int brojac = 0;
 		for (int j = 0; j < kolona; j++)
 		{
 			if (*(*(niz + i) + j)->pozicija == izbor)
 			{
-				prosjeci[i] += *(*(niz + i) + j)->broj_golova;
-				brojaci[i] += 1;
+				*(prosjeci + i) += *(*(niz + i) + j)->broj_golova;
+				brojac++;
 			}
 		}
-		prosjeci[i] /= brojaci[i];
-		if (prosjeci[i] > prosjek_max)
+		*(prosjeci + i) /= brojac;
+
+		if (*(prosjeci + i) > najbolji_prosjek)
 		{
-			prosjek_max = prosjeci[i];
-			indeks_max = i;
+			najbolji_prosjek = *(prosjeci + i);
+			indeks = i;
 		}
 	}
-
-	cout << "Tim sa najvecim prosjekom je tim broj: " << indeks_max+1 << " i njegov prosjek za poziciju " << izbor << " je: " << prosjek_max << endl;
-
-	
+	cout << "Tim sa najvecim prosjekom golova na odabranoj poziciji " << izbor << " je tim " << indeks<<endl;
 }
 
 int main() {
-	int red, kolona;
-	cout << "Unesite dimenzije niza: " << endl;
-	cin >> red >> kolona;
+	int red,  kolona;
+	cout << "Unesite broj timova: ";
+	cin >> red;
+	cout << "Unesite broj igraca: ";
+	cin >> kolona;
+
 	fudbaler** niz = new fudbaler * [red];
 
 	for (int i = 0; i < red; i++)
-		*(niz + i) = new  fudbaler[kolona];
+		*(niz + i) = new fudbaler[kolona];
 
 	Unos(niz, red, kolona);
-	Ispis(niz, red, kolona);
-	Prosjek(niz, red, kolona);
-	Najbolji(niz, red, kolona, 'V');
+	GoloviPoPoziciji(niz, red, kolona);
+	cout << "Unesite poziciju (N, V, O):";
+	char izbor;
+	do
+	{
+		cin >> izbor;
+	} while (izbor!='N' && izbor!='V' && izbor!='O');
+
+	NajboljiProsjekPoPoziciji(niz, red, kolona, izbor);
 
 	for (int i = 0; i < red; i++)
-		delete[] *(niz + i);
+		delete[] * (niz + i);
 
 	delete[] niz;
-	niz = nullptr;
 
 	system("pause>0");
 	return 0;
 }
-
