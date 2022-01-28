22eb8cebf405dcf2103c703ca160afa33e43e58a
naidak <57109234+naidak@users.noreply.github.com>
2022-01-28 13:52:15 +0100

    Update rekurzija zad 2 (infinity git).cpp

--- rekurzija zad 2 (infinity git).cpp
+++ rekurzija zad 2 (infinity git).cpp
@@ -1,58 +1,68 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 /*
-Napisati program koji će omogućiti korisniku da unese veličinu jednodimenzionalnog dinamičkog niza integer vrijednosti.
-Napisati rekurzivnu funkciju koja će taj niz popuniti serijom brojeva 2, 4, 8, 16, 32, 64, 128, 256, 512, itd., 
-tako da je svaki sljedeći broj jednak prethodnom pomnoženom sa 2, počevši od broja 2.
-Zatim,korisnik unosi željeni broj te rekurzivnom funkcijom provjerava da li je taj broj član kreiranog niza.
-Rekurzija se prekida kada se popuni cijeli niz ili kada se desi overflow na integer varijabli.
+	Napisati program koji će omogućiti korisniku da unese veličinu jednodimenzionalnog dinamičkog niza integer vrijednosti.
+	Napisati rekurzivnu funkciju koja će taj niz popuniti serijom brojeva 2, 4, 8, 16, 32, 64, 128, 256, 512, itd., 
+	tako da je svaki sljedeći broj jednak prethodnom pomnoženom sa 2, počevši od broja 2. 
+	Zatim,korisnik unosi željeni broj te rekurzivnom funkcijom provjerava da li je taj broj član kreiranog niza.
+	Rekurzija se prekida kada se popuni cijeli niz ili kada se desi overflow na integer varijabli.
 
-(U ovom zadatku je zabranjeno indeksirati niz uglastim zagradama. Obavezno koristiti aritmetiku pokazivača.)
+	(U ovom zadatku je zabranjeno indeksirati niz uglastim zagradama. Obavezno koristiti aritmetiku pokazivača.)
 */
 
-void Unos(int* niz,int len,int i=0)
-{
-	if (i == len)
+void Unos(int* niz, int vel, int i = 0)
+{	
+	if (i == vel)
 		return;
+	if (i == 0)
+		*(niz) = 2;
 	else {
-		*(niz + i) = pow(2, (i+1));
+		if (2 * *(niz + i - 1) < 0)
+			return;
+		*(niz + i) = 2 * *(niz + i - 1);
 	}
-	Unos(niz, len, i + 1);
+	Unos(niz, vel, i + 1);
 }
 
-void Ispis(int* niz, int len)
+void Ispis(int* niz, int vel)
 {
-	for (int i = 0; i < len; i++)
-		cout << *(niz + i) << " ";
-	cout << endl;
+	if (vel == 0)
+		return;
+	Ispis(niz, vel - 1);
+	cout << niz[vel-1]<<" ";
 }
 
-bool Provjera(int* niz, int len,int broj, int i = 0)
+bool Provjera(int* niz, int vel,int broj)
 {
-	if (i == len)
+	if (vel==0)
 		return false;
-	else if (*(niz + i) == broj)
+	if (*(niz + vel-1) == broj)
 		return true;
-	Provjera(niz, len, broj, i + 1);
+	Provjera(niz, vel-1, broj);
 }
 
 int main() {
-	int len;
-	len = 5;
-	int* niz = new int[len];
-	int trazeni_broj = 32;
-	
-	Unos(niz, len);
+	int n;
+	do
+	{
+		cout << "Unesite velicinu niza: ";
+		cin >> n;
+	} while (n<=2);
 
-	Ispis(niz, len);
+	int* niz = new int[n] {};
 
-	if (Provjera(niz, len, trazeni_broj))
-		cout << trazeni_broj << " postoji u nizu\n";
+	Unos(niz, n);
+	Ispis(niz, n);
+
+	int broj;
+	cout << "Unesite zeljeni broj: ";
+	cin >> broj;
+	if (Provjera(niz, n, broj))
+		cout << "Broj " << broj << " je clan kreiranog niza. \n";
 	else
-		cout << trazeni_broj << " ne postoji u nizu\n";
-
-	delete[] niz;
+		cout << "Broj " << broj << " nije clan kreiranog niza\n";
 
 	system("pause>0");
 	return 0;
